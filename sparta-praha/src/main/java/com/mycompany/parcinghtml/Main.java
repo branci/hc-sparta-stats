8e3b0dac5f0e4f3087e57a7a0084df7ea5de234a
simon <simon@simon-doesTHINK>
2015-05-20 18:06:22 +0200

    Trieda sluziaca na stahovanie udajov o hracoch zo sezon 2005 - 2015 Udaje sa ukladaju do lokalnej databazy V subore pom su uvedene pouzite kniznice

---
+++ sparta-praha/src/main/java/com/mycompany/parcinghtml/Main.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.parcinghtml;
+
+
+import java.io.IOException;
+import java.sql.SQLException;
+import javax.sql.DataSource;
+import org.jsoup.*;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.DERBY;
+
+/**
+ *
+ * @author kristian
+ */
+public class Main {
+    
+    
+     public static DataSource dataSource() {
+       
+        return new EmbeddedDatabaseBuilder()
+                .setType(DERBY)
+                .addScript("classpath:SQLscript.sql")
+                .build();
+    }
+    
+ 
+    
+    public static void main(String[] args) throws SQLException, IOException{
+        DataSource primaryDS = dataSource();
+        
+        
+        
+        ParsingClassPlayers psp = new ParsingClassPlayers(primaryDS);
+        psp.downloadSource();
+        
+        IdioticParsing idioticPars = new IdioticParsing(primaryDS);
+        idioticPars.someParsing();
+   
+    }
+   
+}
